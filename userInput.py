1872732425def50137ed52abebada0f6f7404f65
krister.jaanhold <krister.jaanhold@transferwise.com>
2016-11-04 23:49:36 +0200

    Add loop for soundex differences

--- userInput.py
+++ userInput.py
@@ -1,7 +1,9 @@
 from wcm import *
 from numpy import *
+from soundex import soundex_distance
 import pandas as pd
 import scipy.stats as stats
+import itertools
 from StringIO import StringIO
 
 # Define user inputs
@@ -21,12 +23,6 @@ for string in word_vector:
     except:
         unknown_words.append(string)
 
-print(" ")
-print("Your kid:")
-print("Average WCM score: " + str(round(mean(word_scores),3)))
-print("Maximum WCM score: " + str(round(max(word_scores),3)))
-print("Variance of WCM score: " + str(round(var(word_scores),3)))
-print("Unkown words: " + str(', '.join(str(e) for e in unknown_words)))
 
 # Read in comparison data
 data = pd.read_csv('output.csv',dtype={'data_id':int,'age':int,'sex':object,'mom_ed':object,
@@ -35,6 +31,11 @@ data = pd.read_csv('output.csv',dtype={'data_id':int,'age':int,'sex':object,'mom
 
 # Subset of same age data
 sameage_data = data[data['age']==int(age)]
+
+# Soundex
+soundex_differences = []
+for i,j in itertools.combinations(word_vector,2):
+    soundex_differences.append(soundex_distance(i,j))
 
 # Aggregate scores for each kid
 max_scores = (sameage_data.groupby('data_id')['score'].max()).values.tolist()
@@ -45,6 +46,15 @@ var_scores = (sameage_data.groupby('data_id')['score'].var()).values.tolist()
 max_scores_age = sameage_data.groupby('age')['score'].max().values.tolist()
 avg_scores_age = sameage_data.groupby('age')['score'].mean().values.tolist()
 var_scores_age = sameage_data.groupby('age')['score'].var().values.tolist()
+
+print(" ")
+print("Your kid:")
+print("Average WCM score: " + str(round(mean(word_scores),3)))
+print("Maximum WCM score: " + str(round(max(word_scores),3)))
+print("Variance of WCM score: " + str(round(var(word_scores),3)))
+print("Average pair-wise soundex difference: " + str(round(mean(soundex_differences),2)))
+if unknown_words:
+    print("Unkown words: " + str(', '.join(str(e) for e in unknown_words)))
 
 print(" ")
 print("Average WCM score in this age: " + str(round(avg_scores_age[0],3)))
