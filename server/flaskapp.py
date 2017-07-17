a44d66d5a0a29d6cbc6a6e176f190033dc7c3142
krister.jaanhold <krister.jaanhold@transferwise.com>
2017-07-17 16:41:44 +0300

    Debug

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -35,7 +35,7 @@ def hello_world():
 
 @app.route('/age/')
 def return_age(dob):
-    return(request.args.get('dob'))
+    return(request.data)
 
     #date_object = datetime.strptime(dob, "%Y-%m-%d").date()
     #age = date.today() - date_object
