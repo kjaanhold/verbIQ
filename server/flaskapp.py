d7d47cc2a631d4b6bf4db05b8cdb914a0a00937c
krister.jaanhold <krister.jaanhold@transferwise.com>
2017-07-17 15:18:55 +0300

    Test

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -41,11 +41,7 @@ def return_age(dob):
 
 @app.route("/names/<name>")
 def addnames(name):
-    cur = get_db().cursor()
-    query = 'INSERT INTO names VALUES (%s)' % (name)
-    cur.execute(query)
-    get_db.commit()
-    cur.close()
+    rows = execute_query('INSERT INTO names VALUES (?)', [name.title()])
     return("Inserted " + str(name) + " to table names")
 
 @app.route("/namelist")
