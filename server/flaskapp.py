c20ef5049d4602d26a90a98aded462e9f58732b4
taavitamkivi <taavi.tamkivi@gmail.com>
2017-07-20 10:39:12 +0300

    updated return_age function

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -37,7 +37,7 @@ def hello_world():
 def return_age(dob):
     date_object = datetime.strptime(dob, "%Y-%m-%d").date()
     age = date.today() - date_object
-    out_text = "Su lapse vanus paevades on: " + str(age.days)
+    out_text = "Selge, su laps on: " + str(round(int(age.days)/30)) + " kuud vana. Õige?"
     data = {'messages':[{"text": out_text}]}
     return jsonify(data)
 
