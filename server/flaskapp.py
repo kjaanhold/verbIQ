7e59289d4033fd3ea4d0d69658b258089df892fa
taavitamkivi <taavi.tamkivi@gmail.com>
2017-07-25 23:50:02 +0300

    debugging

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -49,7 +49,7 @@ def age_check():
     name = request.args.get('Lapse_eesnimi')
     date_object = datetime.strptime(dob, "%Y-%m-%d").date()
     age = date.today() - date_object
-    out_text = u"Tänan. " + str(name) + u" sündis " + str(date_object) + " ja ta on praegu " + str(int(round(int(age.days)/30))) + " kuu vanune."
+    out_text = u("Tänan. " + str(name)) + u" sündis " + str(date_object) + " ja ta on praegu " + str(int(round(int(age.days)/30))) + " kuu vanune."
 #    out_text = str(dob) + str(name)
     data = {'messages':[{"text": out_text}]}
     return jsonify(data)
