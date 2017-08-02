76410ca97f54408ea4019d5699eb90aa9273723a
krister.jaanhold <krister.jaanhold@transferwise.com>
2017-08-02 20:44:50 +0300

    Fix path

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -112,7 +112,7 @@ def direct_block_based_on_age(dob):
     data = {"redirect_to_blocks": [next_block]}
     return jsonify(data)
 
-@app.route("/names/", methods = ['POST'])
+@app.route("/names", methods = ['GET','POST'])
 def addnames():
     if request.method == "POST":
         con = connect_to_database()
