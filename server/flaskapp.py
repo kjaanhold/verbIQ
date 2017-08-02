af1b8137e30290b30c2a39a4d26351015aa6a38d
krister.jaanhold <krister.jaanhold@transferwise.com>
2017-08-02 20:59:46 +0300

    Test

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -119,7 +119,8 @@ def addnames():
         cur = con.cursor()
         name = request.form['name']
         query = "INSERT INTO %s VALUES ('%s');" % ('names', name) 
-        cur.execute(query)
+        return(query)
+        #cur.execute(query)
         #con.commit()
         #cur.close()
         return("Inserted " + str(name) + " to table names")
