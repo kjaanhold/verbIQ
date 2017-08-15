f14a133c0c783bfd718412766c54022bac4a821a
krister.jaanhold <krister.jaanhold@transferwise.com>
2017-08-15 19:26:35 +0300

    Fix

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -17,12 +17,12 @@ POSTGRES = {
     'port': '5432',
 }
 
+app = Flask(__name__)
+
+app.config.from_object(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-
-app = Flask(__name__)
-app.config.from_object(__name__)
 
 db.init_app(app)
 
