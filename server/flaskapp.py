883c94ca12efa43e336e795a99d692cd353d5f0d
krister.jaanhold <krister.jaanhold@transferwise.com>
2017-08-15 22:20:48 +0300

    Return

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -59,7 +59,7 @@ def hello_world():
       db.session.commit()
       return 'OK'
     if request.method == "GET":
-      rows = Station.query.all()
+      rows = db.session.query(Station)
       return rows
 
 @app.route('/age/<dob>', methods=['GET'])
