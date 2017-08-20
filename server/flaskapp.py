9379f978c0b81fdb10809340e126f492e452d158
Taavi <taavi.tamkivi@gmail.com>
2017-08-21 00:55:46 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -79,7 +79,7 @@ def store_test_results():
       return 'OK'
 
     if request.method == "GET":
-      data = TestResults.query.first()
+      data = TestResults.query.all()
       return str(data.id_test_result)+";"+str(data.key_user)+";"+str(data.block_name)+";"+str(data.lapse_eesnimi)+";"+str(data.date_created)+";"+str(data.result_type)+";"+str(data.result_value)+";"
 
 
