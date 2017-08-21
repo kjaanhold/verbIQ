eb9c91f3924b71405daff37bfd7d27b83208050f
Taavi <taavi.tamkivi@gmail.com>
2017-08-21 21:05:41 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -117,8 +117,9 @@ def proposenexttest():
       query = "SELECT t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
       text = u"Veel vastamata testid"
 
-    rows = execute_query(query)
-    rows = replace(replace(str(rows),"[",""),"]","")
+    rows = str(execute_query(query))
+    rows = rows.replace("[","")
+    rows = rows.replace("]","")
     data = {"redirect_to_blocks": rows, "selection_criteria": text}    
     return jsonify(data)
 
