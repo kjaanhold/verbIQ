7f67f9a49d997a175c93d1018249befab3f9d7fd
Taavi <taavi.tamkivi@gmail.com>
2017-08-21 22:22:53 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -123,16 +123,17 @@ def proposenexttest():
     out_text = out_text.replace("',)]","")
 #    data = {"redirect_to_blocks": [out_text]} 
 #    out_text = "Testing"
-'''    data = {
+    data = {
       "redirect_to_blocks": [
         out_text
       ]
     }
 '''
     data = {
-      "redirect_to_blocks": rows
+      "redirect_to_blocks": 
+        rows
     }
-
+'''
 
 
     return jsonify(data)
