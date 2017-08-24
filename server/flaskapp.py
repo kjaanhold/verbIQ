d00261ad94002ae553b1608f65c4a957d3c3adbd
Taavi <taavi.tamkivi@gmail.com>
2017-08-24 11:03:30 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -108,7 +108,7 @@ def proposenexttest():
       data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).all()
       json_string = json.dumps(data.block_name)
 
-    return jsonify(data)
+    return str(json_string)
 '''
       data_out = {
         "block_name": data.block_name,
