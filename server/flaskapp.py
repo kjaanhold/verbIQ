de06800b25a932324d0a8ae53911bf18d96eb105
Taavi <taavi.tamkivi@gmail.com>
2017-08-25 00:56:34 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -123,18 +123,12 @@ def proposenexttest():
     if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
       # this kid hasn't done any tests yet
       query = "SELECT t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s ORDER BY RANDOM() LIMIT 1;" % (age_months)
-      text = u"Ei ole veel ühtegi vastust"
       rows = execute_query(query)
       out_text = str(rows)
       out_text = out_text.replace("[(u'","")
       out_text = out_text.replace("',)]","")
 
 #    if not TestResults.query.filter_by(lapse_eesnimi = name.lower(), test_result = 'Ei').first():
-
-
-
-
-
 
     else:
       # this kid has done at least one test
@@ -148,7 +142,6 @@ def proposenexttest():
       block_name = block_name.replace(']','')
 
       query = "SELECT t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
-      text = u"Veel vastamata testid"
 
       rows = execute_query(query)
       out_text = str(rows)
@@ -156,7 +149,22 @@ def proposenexttest():
       out_text = out_text.replace("',)]","")
 
       if out_text == '[]':
-        out_text = 'Default answer'
+        # now focus only on non-Jah answers:
+        data = TestResults.query.filter_by(lapse_eesnimi = name.lower(), test_result != 'Jah').all()
+        result_dict = [u.__dict__ for u in data]
+        block_name = [d.get('block_name') for d in result_dict]
+        block_name = str(block_name)
+        block_name = block_name.replace('u"','')
+        block_name = block_name.replace('"','')
+        block_name = block_name.replace('[','')
+        block_name = block_name.replace(']','')
+
+        # tests that have got non-Jah results previously
+        query = "SELECT t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
+        rows = execute_query(query)
+        out_text = str(rows)
+        out_text = out_text.replace("[(u'","")
+        out_text = out_text.replace("',)]","")
 
     data = {
       "redirect_to_blocks": [
