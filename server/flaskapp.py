a1f0bfbf333e85fb8a44d37e03ef6b4bd3c824ef
Taavi <taavi.tamkivi@gmail.com>
2017-08-24 22:54:35 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -131,7 +131,7 @@ def proposenexttest():
       data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).all()
       result_dict = [u.__dict__ for u in data]
 #      out = str(result_dict)
-      out = json(result_dict['block_name'])
+      out = str(result_dict[0]['block_name'])
 
 
 #      data_str = str(data)
