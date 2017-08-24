0922338244f1a721a6568b5d3a8dcdda0e819267
Taavi <taavi.tamkivi@gmail.com>
2017-08-24 11:37:10 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -106,7 +106,7 @@ def proposenexttest():
 
     else:
       # this kid has done at least one test
-      data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).first()
+      data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).all()
       data_out = {
 #        "block_name": data.block_name,
         "result_value": data.result_value
