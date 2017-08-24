eb47a9e5fe4afe8873702b5347ff681d8d913dff
Taavi <taavi.tamkivi@gmail.com>
2017-08-24 23:28:46 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -135,7 +135,7 @@ def proposenexttest():
 #      out = str(result_dict[0]['block_name']) + ',' + str(result_dict[1]['block_name'])
 
       out = ''
-      for i in range (0,2):
+      for i in range (0,5):
         out = out + str(result_dict[i]['block_name']) + ','
 #      out.extend(s)
 #      out = "tes"
