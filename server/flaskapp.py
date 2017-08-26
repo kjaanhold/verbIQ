572483024139338aede6c26437e8ef8c4ed9d1be
Taavi <taavi.tamkivi@gmail.com>
2017-08-26 12:00:10 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -198,6 +198,7 @@ def run_test():
       query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s ORDER BY RANDOM() LIMIT 1;" % (age_months)
       rows = execute_query(query)
   
+  '''
       next_test = out_text.split(", ")[0]
       next_test = next_test.replace("[(u'","")
       next_test = next_test.replace("',)]","")
@@ -205,7 +206,9 @@ def run_test():
       block_name = out_text.split(", ")[1]
       block_name = block_name.replace("u'","")
       block_name = block_name.replace("')]","")
-
+'''
+      next_test = "a"
+      block_name = "b"
 #    if not TestResults.query.filter_by(lapse_eesnimi = name.lower(), test_result = 'Ei').first():
 
     else:
