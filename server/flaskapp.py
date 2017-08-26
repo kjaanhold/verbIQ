869b1cc16b91c0c95676d4d891801ce00be84cf1
Taavi <taavi.tamkivi@gmail.com>
2017-08-26 11:44:17 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -219,11 +219,12 @@ def run_test():
       block_name = block_name.replace('[','')
       block_name = block_name.replace(']','')
 
+
       query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
       text = u"Veel vastamata testid"
 
       rows = execute_query(query)
-      out_text = str(rows)
+'''      out_text = str(rows)
 
       next_test = out_text.split("', ")[0]
       next_test = next_test.replace("[(u'","")
@@ -232,6 +233,9 @@ def run_test():
       block_name = out_text.split("', ")[1]
       block_name = block_name.replace("u'","")
       block_name = block_name.replace("')]","")
+'''
+      next_test = rows[0][0]
+      block_name = rows[0][1]
 
 #      if out_text == '[]':
 #        out_text = 'Default answer' 
