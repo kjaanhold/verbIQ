469356261b54997f129131946509c0606e997b68
Taavi <taavi.tamkivi@gmail.com>
2017-08-26 20:40:18 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -221,35 +221,13 @@ def run_test():
       out_text = str(rows)
 
       if out_text == '[]':
-
-        # has this kid got any "ei" answers?
-        data = TestResults.query.filter_by(lapse_eesnimi = name.lower(), result_value = 'Ei').all()
-        result_dict = [u.__dict__ for u in data]
-        block_name = [d.get('block_name') for d in result_dict]
-        block_name = str(block_name)
-        block_name = block_name.replace('u"','')
-        block_name = block_name.replace('"','')
-        block_name = block_name.replace('[','')
-        block_name = block_name.replace(']','')
-
-        query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
-        rows = execute_query(query)
-        out_text = str(rows)
-
-        next_test = out_text.split("', ")[0]
-        next_test = next_test.replace("[(u'","")
-        next_test = next_test.replace("',)]","")
-
-        block_name = out_text.split("', ")[1]
-        block_name = block_name.replace("u'","")
-        block_name = block_name.replace("')]","")
-
-
-      if out_text == '[]':
         next_test = 'a'
-        block_name = 'Default answer'
+        block_name = 'b'
 
       else:
+
+#      next_test = repr(rows[0][0])
+#      block_name = str(rows[0][1])
 
         next_test = out_text.split("', ")[0]
         next_test = next_test.replace("[(u'","")
