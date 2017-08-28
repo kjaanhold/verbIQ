5053b6e5044f34d5e95c9b6e3625be5cf7d5a7b7
Taavi <taavi.tamkivi@gmail.com>
2017-08-28 13:36:21 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -282,13 +282,10 @@ def return_test_results(name, result_value):
 
       query = "SELECT m.description FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
       rows = execute_query(query)
-
       out_text = rows # "\n".join(.join(elems) for elems in rows)
-
-
     return str(out_text)
 
-
+'''
 @app.route('/tests_summary', methods=['GET'])
 def tests_summary():
     name = request.args.get('Lapse_eesnimi')
@@ -381,7 +378,7 @@ def tests_summary():
 
     response = Response(json.dumps(data,ensure_ascii = False), content_type="application/json; charset=utf-8")
     return response
-
+'''
 
 
 
