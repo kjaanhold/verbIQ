24e093389ab0e8f88013a1523032a1030e80e455
Taavi <taavi.tamkivi@gmail.com>
2017-08-28 13:46:05 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -139,9 +139,9 @@ def next_test_selection(dob,name):
       block_name = block_name.replace('[','')
       block_name = block_name.replace(']','')
 
-      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
-      rows = execute_query(query)
-      out_text = str(rows)
+   #   query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
+   #   rows = execute_query(query)
+   #   out_text = str(rows)
 
       if out_text == '[]':
         question  = 'done'
