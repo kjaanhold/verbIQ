a716037bfab39f337b72230c60d8c258cd02ecfe
Taavi <taavi.tamkivi@gmail.com>
2017-08-30 22:18:45 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -166,6 +166,8 @@ def next_test_selection(dob,name):
     age = date.today() - date_object
     age_months = str(int(age.days)/30)
 
+#    if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
+
     if 3 < 2:
       # this kid hasn't done any tests yet
 
@@ -177,18 +179,18 @@ def next_test_selection(dob,name):
 
     else:
       # this kid has done at least one test
-#      data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).all()
-
-#      result_dict = [u.__dict__ for u in data]
-#      block_name = [d.get('block_name') for d in result_dict]
-      block_name = "adfaddfa"
+      data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).all()
+      result_dict = [u.__dict__ for u in data]
+      block_name = [d.get('block_name') for d in result_dict]
       block_name = str(block_name)
       block_name = block_name.replace('u"','')
       block_name = block_name.replace('"','')
       block_name = block_name.replace('[','')
       block_name = block_name.replace(']','')
 
-      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
+#      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
+      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s ORDER BY RANDOM() LIMIT 1;" % (age_months)
+
       rows = execute_query(query)
       out_text = str(rows)
 
