2ed14db58b9185c31d9904fd550d46cc45911e26
Taavi <taavi.tamkivi@gmail.com>
2017-08-30 22:19:49 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -178,18 +178,6 @@ def next_test_selection(dob,name):
       block_name = str(rows[0][1])
 
     else:
-      # this kid has done at least one test
-'''
-      data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).all()
-      result_dict = [u.__dict__ for u in data]
-      block_name = [d.get('block_name') for d in result_dict]
-      block_name = str(block_name)
-      block_name = block_name.replace('u"','')
-      block_name = block_name.replace('"','')
-      block_name = block_name.replace('[','')
-      block_name = block_name.replace(']','')
-'''
-#      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
       query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s ORDER BY RANDOM() LIMIT 1;" % (age_months)
 
       rows = execute_query(query)
