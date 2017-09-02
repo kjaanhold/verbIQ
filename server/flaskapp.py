92979ff0c49b5d548de8eccd1d0fae3e610af2fc
Taavi <taavi.tamkivi@gmail.com>
2017-09-02 17:34:40 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -120,8 +120,9 @@ def next_test_selection(dob,name):
     age_months = str(int(age.days)/30)
 
 ###    return str(age)
-    if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
+#    if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
 
+    if 2 > 3:
       # this kid hasn't done any tests yet
 
       query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s ORDER BY RANDOM() LIMIT 1;" % (age_months)
