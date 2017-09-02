532bc6a4241a7b45cb42b88a81fc095447617f21
Taavi <taavi.tamkivi@gmail.com>
2017-09-02 17:46:32 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -117,7 +117,7 @@ def next_test_selection():
     dob = request.args.get('Synni_kuupaev')
     name = request.args.get('Lapse_eesnimi')
 
-    data = [str(dob), str(name)]
+    data = ["a", "b"]
     return data
 
 '''
