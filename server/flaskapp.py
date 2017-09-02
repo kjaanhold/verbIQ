a47364bceddd69490117d745ae56165f0b7f2d2a
Taavi <taavi.tamkivi@gmail.com>
2017-09-02 17:42:59 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -114,12 +114,6 @@ def to_json(inst, cls):
 
 @app.route('/next_test_selection', methods=['GET'])
 def next_test_selection():
-    dob = request.args.get('Synni_kuupaev')
-    name = request.args.get('Lapse_eesnimi')
-
-    date_object = datetime.strptime(dob, "%Y-%m-%d").date()
-    age = date.today() - date_object
-    age_months = str(int(age.days)/30)
 
     question = "What's your name?"
     block_name = "dummy_question"
