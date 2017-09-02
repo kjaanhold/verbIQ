700463139b0cfd6c773d2955d716e3ce9d94c256
Taavi <taavi.tamkivi@gmail.com>
2017-09-02 17:54:01 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -155,16 +155,6 @@ def next_test_selection():
         question = "What's your name?"
         block_name = "dummy_question"
 
-'''
-        question = out_text.split("', ")[0]
-        question = question.replace("[(u'","")
-        question = question.replace("',)]","")
-
-        block_name = out_text.split("', ")[1]
-        block_name = block_name.replace("u'","")
-        block_name = block_name.replace("')]","")
-'''
-#    data = [question,block_name]
     data = str(question) + str(block_name)
 
     return data
