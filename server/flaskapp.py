67c170ff4b22c13bebccf632043074e2441cffbd
Taavi <taavi.tamkivi@gmail.com>
2017-09-02 17:37:53 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -159,6 +159,9 @@ def next_test_selection(dob,name):
         block_name = block_name.replace("u'","")
         block_name = block_name.replace("')]","")
 
+    data = [question,block_name]
+    return data
+
 
 
 
