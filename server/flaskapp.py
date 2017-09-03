aa4b2d7ccf8680ef6c537fa5fcbe22456c91babf
Taavi <taavi.tamkivi@gmail.com>
2017-09-03 12:50:21 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -131,7 +131,7 @@ def next_test_selection_2():
     question = repr(rows[0][0])
     question = question.replace("u'","")
 
-    return str(repr(rows[0][0]))
+    return str(repr(rows[0][0]).encode('cp437'))
 
 # @app.route('/next_test_selection', methods=['GET'])
 def next_test_selection(dob, name):
