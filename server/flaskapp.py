7ea9f13dbd363b8b9c5695d750cbd94202269083
Taavi <taavi.tamkivi@gmail.com>
2017-09-03 13:49:57 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -135,7 +135,7 @@ def next_test_selection_2():
 #    question = u'Kas {{Lapse_eesnimi}} j\xe4lgib liikuvaid esemeid?'
 #    question = question.encode('string-escape')
 
-    return function_that_prints(question)
+    return str(question)
 
 # @app.route('/next_test_selection', methods=['GET'])
 def next_test_selection(dob, name):
