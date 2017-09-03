5721428aeb093bef3d7ab5e851b77fd710241196
Taavi <taavi.tamkivi@gmail.com>
2017-09-03 18:43:59 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -116,9 +116,9 @@ def function_that_prints(a):
 
 
 @app.route('/next_test_selection', methods=['GET'])
-def next_test_selection_2():
-    dob = request.args.get('Synni_kuupaev')
-    name = request.args.get('Lapse_eesnimi')
+def next_test_selection_2(dob,name):
+#    dob = request.args.get('Synni_kuupaev')
+#    name = request.args.get('Lapse_eesnimi')
 
     date_object = datetime.strptime(dob, "%Y-%m-%d").date()
     age = date.today() - date_object
@@ -222,8 +222,8 @@ def run_test():
 
     selected_test = next_test_selection(dob = dob, name = name)
 
-    question = str(selected_test.split("//")[0])
-    block_name = str(selected_test.split("//")[1])
+    question = str(selected_test.split("///")[0])
+    block_name = str(selected_test.split("///")[1])
 
 
     if question == "done":
