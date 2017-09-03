56345ab43a78b5a685fc44705c64c9936f2760be
Taavi <taavi.tamkivi@gmail.com>
2017-09-03 21:20:08 +0300

    “debugging”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -145,7 +145,7 @@ def next_test_selection():
       block_name = block_name.replace(']','')
 
       query = "SELECT t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
-      rows = execute_query(query)
+#      rows = execute_query(query)
 
 
 '''
