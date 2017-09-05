e7a52e324b280536f6cbcc618532ec9c2b512834
Taavi <taavi.tamkivi@gmail.com>
2017-09-05 21:08:11 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -130,7 +130,7 @@ def next_test_selection(dob,name):
       # this kid hasn't done any tests yet
     if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
 
-      query = "SELECT t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE %s >= (2*tm.target_age)/3  AND %s <= (4*m.target_age)/3 ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months)
+      query = "SELECT t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= (4*%s)/3 AND m.target_age >= (2*%s)/3 ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months)
       rows = execute_query(query)
       question = str(rows[0][0].encode("utf-8"))
       block_name = str(rows[0][1].encode("utf-8"))
@@ -147,7 +147,7 @@ def next_test_selection(dob,name):
       block_name = block_name.replace('[','')
       block_name = block_name.replace(']','')
 
-      query = "SELECT t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE %s >= (2*tm.target_age)/3  AND %s <= (4*m.target_age)/3 AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months, block_name)
+      query = "SELECT t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE tm.target_age <= (4*%s)/3 AND m.target_age >= (2*%s)/3 AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months, block_name)
       rows = execute_query(query)
 
       if str(rows) == '[]':
