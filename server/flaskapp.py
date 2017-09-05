f87d29f232d8191d4187ad26a705d705dedf9598
Taavi <taavi.tamkivi@gmail.com>
2017-09-05 16:17:34 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -291,7 +291,7 @@ def return_test_results():
       query = "SELECT m.description FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
       rows = execute_query(query)
 #      rows = [d.get('block_name') for d in result_dict]    
-
+      rows = [x for x,_ in rows]
 
 
       out_text = rows[0] # "\n".join(.join(elems) for elems in rows)
