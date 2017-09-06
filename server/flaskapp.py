07def0bf4587adef7547978e13a01e757d2823a4
Taavi <taavi.tamkivi@gmail.com>
2017-09-06 21:44:14 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -284,6 +284,7 @@ def return_test_results(name, result_value):
 #    name = request.args.get('Lapse_eesnimi')
 #    result_value = request.args.get('result_value')
 
+
     if not TestResults.query.filter_by(lapse_eesnimi = name.lower(), result_value = result_value).first():
       out_text = "no_results"
 
@@ -310,6 +311,9 @@ def return_test_results(name, result_value):
 
 @app.route('/tests_summary', methods=['GET'])
 def tests_summary():
+
+    return str("saa")
+'''  
     name = request.args.get('Lapse_eesnimi')
     if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
       out_text = u"Ühtegi testi pole veel tehtud"
@@ -403,7 +407,7 @@ def tests_summary():
 
     response = Response(json.dumps(data,ensure_ascii = False), content_type="application/json; charset=utf-8")
     return response
-
+'''
 
 
 
