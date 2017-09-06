e766dc71fbc40a691baee38aa4bc8f303e3aa417
Taavi <taavi.tamkivi@gmail.com>
2017-09-06 12:06:16 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -3,13 +3,13 @@
 import csv
 import sqlite3
 import json
-import scipy
+#import scipy
 
 from flask import Flask, request, g, jsonify, Response
 from datetime import datetime, date
 from models import db, Station, TestResults
 from sqlalchemy import exc
-# from scipy.stats import norm
+from scipy.stats import norm
 
 
 DATABASE = '/home/ubuntu/verbIQ/server/verbiq.db'
