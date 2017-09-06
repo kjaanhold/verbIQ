6fca08ca66d669e80e81efdf56974d9daff70cfb
Taavi <taavi.tamkivi@gmail.com>
2017-09-06 21:57:34 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -316,11 +316,11 @@ def tests_summary():
 
     out_text = str(name)
 
-    if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
-#    if 2<3:
+#    if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
+    if 2<3:
       out_text = u"Ühtegi testi pole veel tehtud"
 
-    return str("saa")
+    return str(out_text)
 '''  
 
     else:
