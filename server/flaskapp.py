acda97ceb8216aadb350e375ab6217aaab8d7f83
Taavi <taavi.tamkivi@gmail.com>
2017-09-07 09:59:39 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -286,7 +286,7 @@ def return_test_results():
     out_text = str(name)
 
 
-    out_text = str(TestResults.query.filter_by(lapse_eesnimi = name.lower(), result_value = result_value).first())
+#    out_text = str(TestResults.query.filter_by(lapse_eesnimi = name.lower(), result_value = result_value).first())
 #    if not TestResults.query.filter_by(lapse_eesnimi = name.lower(), result_value = result_value).first():
 #    if 2>3:
 #      out_text = "no_results"
@@ -454,76 +454,6 @@ def getmilestones():
     query = "SELECT target_age, description FROM milestones WHERE target_age <= %s;" % age_months
     rows = execute_query(query)
     return(str(rows) + "\n")
-
-
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class BaseModel(db.Model):
-    """Base data model for all objects"""
-    __abstract__ = True
-
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def __repr__(self):
-        """Define a base way to print models"""
-        return '%s(%s)' % (self.__class__.__name__, {
-            column: value
-            for column, value in self._to_dict().items()
-        })
-
-    def json(self):
-        """
-                Define a base way to jsonify models, dealing with datetime objects
-        """
-        return {
-            column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
-            for column, value in self._to_dict().items()
-        }
-
-
-class Station(BaseModel, db.Model):
-    """Model for the stations table"""
-    __tablename__ = 'stations'
-
-    id = db.Column(db.Integer, primary_key = True)
-    lat = db.Column(db.Float)
-    lng = db.Column(db.Float)
-
-    def __init__(self, lat, lng):
-      self.lat = lat
-      self.lng = lng
-
-class TestResults(BaseModel, db.Model):
-    """Model for the test_results table"""
-    __tablename__ = 'test_results'
-
-    id_test_result = db.Column(db.Integer, primary_key = True)
-    key_user = db.Column(db.String)
-    block_name = db.Column(db.String)
-    lapse_eesnimi = db.Column(db.String)
-    date_created = db.Column(db.DateTime)
-    result_type = db.Column(db.String)
-    result_value = db.Column(db.String)
-
-    def __init__(self,key_user,block_name,lapse_eesnimi,date_created,result_type,result_value):
-        self.key_user = key_user
-        self.block_name = block_name
-        self.lapse_eesnimi = lapse_eesnimi
-        self.date_created = date_created
-        self.result_type = result_type
-        self.result_value = result_value
-
-    @property
-    def json(self):
-        return to_json(self, self.__class__)
-
-
-
-
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
