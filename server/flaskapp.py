97b6c3d2bb34bf2badef81a1d795b6e9507b27e8
krister.jaanhold <krister.jaanhold@transferwise.com>
2017-09-07 22:11:29 +0300

    Fix extra bracket

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -286,7 +286,7 @@ def return_test_results(name, result_value):
 
       query = "SELECT m.description FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
       rows = execute_query(query)
-      out_text = rows[0][0].encode("utf-8"))
+      out_text = rows[0][0].encode("utf-8")
 
       query = "SELECT count(distinct m.description) FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
       rows = execute_query(query)
