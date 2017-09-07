bb07b18e3338986d90f67bdd144f5e03f0b70720
Taavi <taavi.tamkivi@gmail.com>
2017-09-07 09:36:21 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -286,9 +286,10 @@ def return_test_results():
     out_text = "null"
 
 
+    out_text = str(TestResults.query.filter_by(lapse_eesnimi = name.lower(), result_value = result_value).first())
 #    if not TestResults.query.filter_by(lapse_eesnimi = name.lower(), result_value = result_value).first():
-    if 2>3:
-      out_text = "no_results"
+#    if 2>3:
+#      out_text = "no_results"
 
     return str(out_text)
 '''    
