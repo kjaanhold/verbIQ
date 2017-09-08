e6b437f0899356a06b37365435ed932aa87f3c99
Taavi <taavi.tamkivi@gmail.com>
2017-09-08 11:32:57 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -124,7 +124,7 @@ def next_test_selection():
       # this kid hasn't done any tests yet
     if TestResults.query.filter_by(lapse_eesnimi = name.lower()).first() is None:
 
-      query = "SELECT t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= (4*%s)/3 AND m.target_age >= (2*%s)/3 ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months)
+      query = "SELECT 1;" #t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= (4*%s)/3 AND m.target_age >= (2*%s)/3 ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months)
       rows = execute_query(query)
 
     else:
