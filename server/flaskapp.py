02f1ff4dc8b18571979eee47b1858338b63c45c2
Taavi <taavi.tamkivi@gmail.com>
2017-09-08 12:13:16 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -133,7 +133,7 @@ def next_test_selection():
       if rows == '':
         query = u"tühi string"
 
-      elif rows.rowcount == 0:
+      elif len(rows) == 0:
         query = u"tühi list"
 
       else:
