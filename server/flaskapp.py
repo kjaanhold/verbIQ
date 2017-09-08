ef2d5e4261381c4c030a268d2839dc110d1721a0
Taavi <taavi.tamkivi@gmail.com>
2017-09-08 10:11:24 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -296,7 +296,10 @@ def return_test_results(name, result_value):
 @app.route('/tests_summary', methods=['GET'])
 def tests_summary():
     name = request.args.get('Lapse_eesnimi')
-    
+
+    return str(TestResults.query.filter_by(lapse_eesnimi = name.lower()).first())
+
+'''    
     if not TestResults.query.filter_by(lapse_eesnimi = name.lower()).first():
       out_text = u"Ühtegi testi pole veel tehtud"
 
@@ -386,6 +389,7 @@ def tests_summary():
 
     response = Response(json.dumps(data,ensure_ascii = False), content_type="application/json; charset=utf-8")
     return response
+'''
 
 @app.route('/age_block_selection/<dob>', methods=['GET'])
 def direct_block_based_on_age(dob):
