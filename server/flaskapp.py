cfd9d21b80ba0caf381917429c3e6854efcfe16a
Taavi <taavi.tamkivi@gmail.com>
2017-09-10 21:49:19 +0300

    “test”

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -128,7 +128,7 @@ def next_test_selection():
 
       query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= (4*%s)/3 AND m.target_age >= (2*%s)/3 ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months)
 
-      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test ORDER BY RANDOM() LIMIT 1;"
+      query = "SELECT t.description, t.block_name FROM tests t ORDER BY RANDOM() LIMIT 1;"
 
       rows = execute_query(query)
 
