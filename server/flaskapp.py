09203ff4d6b652bd639b59d5b8a7ef315ee015e3
Taavi <taavi.tamkivi@gmail.com>
2017-09-11 11:20:18 +0300

    debugging

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -8,7 +8,7 @@ from datetime import datetime, date
 from models import db, Station, TestResults
 from sqlalchemy import exc
 from sqlalchemy.orm.exc import NoResultFound
-
+from scipy.stats import norm
 
 
 DATABASE = '/home/ubuntu/verbIQ/server/verbiq.db'
