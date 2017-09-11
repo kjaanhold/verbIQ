a5dc6b6a7d9e9850dba6689cae2755e80b448cd1
Taavi <taavi.tamkivi@gmail.com>
2017-09-11 08:32:10 +0300

    debugging

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -131,6 +131,18 @@ def next_test_selection():
       block_name = str(rows[0][1].encode("utf-8"))
 
     else:
+      # this kid has done at least one test
+      data = TestResults.query.filter_by(lapse_eesnimi = name.lower()).all()
+      result_dict = [u.__dict__ for u in data]
+      block_name = [d.get('block_name') for d in result_dict]
+      block_name = str(block_name)
+      block_name = block_name.replace('u"','')
+      block_name = block_name.replace('"','')
+      block_name = block_name.replace("u'","'")
+      block_name = block_name.replace('[','')
+      block_name = block_name.replace(']','')
+ 
+      query = "SELECT t.description, t.block_name, t.id_test FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age <= %s AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, block_name)    
       rows = execute_query(query)
 
       if str(rows) == '[]':
@@ -142,6 +154,9 @@ def next_test_selection():
         block_name = str(rows[0][1].encode("utf-8"))
 
     return str(question) + '///' + str(block_name)
+
+
+
 
 
 
