7df712776c7f33cba2ae56ee115e7a429d02ebbc
Taavi <taavi.tamkivi@gmail.com>
2017-09-11 11:46:44 +0300

    debug

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -297,15 +297,15 @@ def return_test_results():
       block_name = block_name.replace('[','')
       block_name = block_name.replace(']','')
 
-      query = "SELECT m.description FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
-      rows = execute_query(query)
-      out_text = rows[0][0].encode("utf-8")
+#      query = "SELECT m.description FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
+#      rows = execute_query(query)
+#      out_text = rows[0][0].encode("utf-8")
 
       query = "SELECT count(distinct m.description) FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
       rows = execute_query(query)
       out_text = rows[0][0]
 
-    return str(query)
+    return str(name) + str(result_value)
 
 
 @app.route('/tests_summary', methods=['GET'])
