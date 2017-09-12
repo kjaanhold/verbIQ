a7ace96a9a2033b73f29bdf6f0876482268a7ede
Taavi <taavi.tamkivi@gmail.com>
2017-09-12 19:40:24 +0300

    d

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -305,7 +305,7 @@ def return_test_results():
       block_name = block_name.replace('[','')
       block_name = block_name.replace(']','')
 
-      query = "SELECT m.description FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
+      query = "SELECT m.target_month, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE t.block_name IN (%s);" % (block_name)    
 #      rows = execute_query(query)
 #      out_text = rows[0][0].encode("utf-8")
 
