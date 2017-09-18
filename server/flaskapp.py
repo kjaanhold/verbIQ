ff76b2e96ef468e62940d6d4f61025e15de64ba8
Taavi <taavi.tamkivi@gmail.com>
2017-09-18 22:58:00 +0300

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -407,13 +407,13 @@ def tests_summary():
       elif (str(data_jah) != 'no_results' and str(data_ei) != 'no_results'):
         data = {
             "messages": [
-              {"text": str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah},
+              {"text": str(name) + " on " + age_months + u" kuu vanune ja ta oskab " + data_jah},
               {
                 "attachment": {
                   "type": "template",
                   "payload": {
                     "template_type": "button",
-                    "text": str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah,
+                    "text": u"aga ta ei oska hästi veel ise " + data_ei,
                     "buttons": [
                       {
                         "type": "show_block",
@@ -426,6 +426,7 @@ def tests_summary():
               }
             ]
           }
+
 
 
     response = Response(json.dumps(data,ensure_ascii = False), content_type="application/json; charset=utf-8")
