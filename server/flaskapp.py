80d33c8ef53e5cb57b296508423040aee691f9a0
Taavi <taavi.tamkivi@gmail.com>
2017-09-18 22:51:48 +0300

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -383,9 +383,6 @@ def tests_summary():
       data_ei_tea = str(return_test_results(name, 'Ei tea'))
 
       if (str(data_jah) != 'no_results' and str(data_ei) == 'no_results'):
-
-        out_text = str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah
-  
         data = {
             "messages": [
               {
@@ -393,7 +390,7 @@ def tests_summary():
                   "type": "template",
                   "payload": {
                     "template_type": "button",
-                    "text": out_text,
+                    "text": str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah,
                     "buttons": [
                       {
                         "type": "show_block",
@@ -406,6 +403,74 @@ def tests_summary():
               }
             ]
           }
+
+      elif (str(data_jah) != 'no_results' and str(data_ei) != 'no_results'):
+        data = {
+            "messages": [
+              {"text": str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah},
+              {
+                "attachment": {
+                  "type": "template",
+                  "payload": {
+                    "template_type": "button",
+                    "text": "aga ta ei oska hästi veel ise " + data_ei,
+                    "buttons": [
+                      {
+                        "type": "show_block",
+                        "block_name": "test recurring tests 2",
+                        "title": "Tagasi testima"
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+
+      elif (str(data_jah) == 'no_results' and str(data_ei) != 'no_results'):
+        data = {
+            "messages": [
+              {
+                "attachment": {
+                  "type": "template",
+                  "payload": {
+                    "template_type": "button",
+                    "text": str(name) + " on " + age_months + " kuu vanune ja ta ei oska hästi veel " + data_jah
+                    "buttons": [
+                      {
+                        "type": "show_block",
+                        "block_name": "test recurring tests 2",
+                        "title": "Tagasi testima"
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+
+      else:
+        data = {
+            "messages": [
+              {
+                "attachment": {
+                  "type": "template",
+                  "payload": {
+                    "template_type": "button",
+                    "text": "viga!"
+                    "buttons": [
+                      {
+                        "type": "show_block",
+                        "block_name": "test recurring tests 2",
+                        "title": "Tagasi testima"
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+
 
     response = Response(json.dumps(data,ensure_ascii = False), content_type="application/json; charset=utf-8")
     return response
