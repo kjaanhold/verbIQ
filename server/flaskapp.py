7f3e0d20721b09f358bb317e117aa8df52201842
Taavi <taavi.tamkivi@gmail.com>
2017-10-05 11:37:03 +0300

    a
    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -104,25 +104,29 @@ def store_children():
       gender = request.form['Lapse_sugu']
       first_updated = datetime.utcnow()
       last_updated = datetime.utcnow()
-
-      if TestResults.query.filter_by(lapse_eesnimi = lapse_eesnimi.lower()).count() == 0:
-        new_data = Children(key_user=str(key_user), lapse_eesnimi=str(lapse_eesnimi.encode('utf8')).lower(), date_of_birth=str(date_of_birth), gender=str(gender), first_updated=str(first_updated), last_updated=str(last_updated))
-        db.session.add(new_data)
-        db.session.commit()
-        a = TestResults.query.filter_by(lapse_eesnimi = lapse_eesnimi.lower()).first()
-        data = {"redirect_to_blocks": ["inserted: "+ str(a)]}
-        return jsonify(data)
-
-      else:
-        child = Children.query.filter_by(lapse_eesnimi = lapse_eesnimi).first()
-        child.last_updated = datetime.utcnow()
-        db.session.commit()
-        a = TestResults.query.filter_by(lapse_eesnimi = lapse_eesnimi).first()
-        data = {"redirect_to_blocks": ["updated: " + str(a)]}
-        return jsonify(data)
+      new_data = Children(key_user=str(key_user), lapse_eesnimi=str(lapse_eesnimi.encode('utf8')).lower(), date_of_birth=str(date_of_birth), gender=str(gender), first_updated=str(first_updated), last_updated=str(last_updated))
+      db.session.add(new_data)
+      db.session.commit()
+      data = {"redirect_to_blocks": ["test recurring tests 2"]}
+      return jsonify(data)
 
     if request.method == "GET":
       data = {'messages':[{"text": "error: "}]}
+      return jsonify(data)
+
+
+@app.route('/return_children', methods = ['GET'])
+def return_children(key_user):
+
+    if Children.query.filter_by(key_user = key_user).first() is None:
+      data = {"redirect_to_blocks": ["yhtegi last pole veel sisestatud"]}
+      return jsonify(data)
+
+    else:
+      child = Children.query.filter_by(key_user = key_user).first()
+      child.last_updated = datetime.utcnow()
+      db.session.commit()
+      data = {'messages':[{"text": "lapsed on: ...: "}]}
       return jsonify(data)
 
 
