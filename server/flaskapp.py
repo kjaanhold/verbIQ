13ae464d4cc3f5ab111921d109711d6c60aad12f
Taavi <taavi.tamkivi@gmail.com>
2017-10-05 10:53:03 +0300

    de
    de

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -109,23 +109,19 @@ def store_children():
         new_data = Children(key_user=str(key_user), lapse_eesnimi=str(lapse_eesnimi.encode('utf8')).lower(), date_of_birth=str(date_of_birth), gender=str(gender), first_updated=str(first_updated), last_updated=str(last_updated))
         db.session.add(new_data)
         db.session.commit()
+        data = {"redirect_to_blocks": ["inserted"]}
 
-      data = {"redirect_to_blocks": ["test recurring tests 2"]}
+      else:
+        child = Children.query.filter_by(key_user = "muu").first()
+        child.last_updated = datetime.utcnow()
+        db.session.commit()
+        data = {"redirect_to_blocks": ["updated"]}
+
       return jsonify(data)
 
     if request.method == "GET":
       data = {'messages':[{"text": "error: "}]}
       return jsonify(data)
-
-
-@app.route('/update_children', methods = ['GET'])
-def update_children():
-
-    child = Children.query.filter_by(key_user = "muu").first()
-    child.last_updated = datetime.utcnow()
-    db.session.commit()
-    data = {'messages':[{"text": "updated: "}]}
-    return jsonify(data)
 
 
 def to_json(inst, cls):
