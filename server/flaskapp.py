05642e6749a3a34b7f7d584e3478456f13cf6a91
Taavi <taavi.tamkivi@gmail.com>
2017-10-09 14:57:52 +0300

    create CDF

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -76,7 +76,7 @@ def store_test_results():
       result_type = 'chatfuel'
       result_value = request.form['test_result']
 
-      new_data = TestResults(key_user=str(key_user), block_name=str("'"+block_name+"'"), lapse_eesnimi=str(lapse_eesnimi.encode('utf8')).lower(), date_created=str(date_created), result_type=str(result_type), result_value=str(result_value))
+      new_data = TestResults(key_user=str(key_user), block_name=str("'"+block_name+"'"), lapse_eesnimi=str(lapse_eesnimi.encode('utf8')), date_created=str(date_created), result_type=str(result_type), result_value=str(result_value))
       db.session.add(new_data)
       db.session.commit()
 
@@ -325,7 +325,7 @@ def next_test_selection(dob,name):
       # this kid hasn't done any tests yet
     if TestResults.query.filter_by(lapse_eesnimi = name.lower()).first() is None:
 
-      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age BETWEEN (2*%s)/3 AND (4*%s)/3 ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months)
+      query = "SELECT t.description, t.block_name, m.target_age FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE m.target_age BETWEEN (2*%s)/3 AND (4*%s)/3 ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months)
       rows = execute_query(query)
 
       question = str(rows[0][0].encode("utf-8"))
@@ -343,19 +343,21 @@ def next_test_selection(dob,name):
       block_name = block_name.replace('[','')
       block_name = block_name.replace(']','')
  
-      query = "SELECT t.description, t.block_name FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE ( m.target_age BETWEEN (2*%s)/3 AND (4*%s)/3 ) AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months, block_name)    
+      query = "SELECT t.description, t.block_name, m.target_age FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE ( m.target_age BETWEEN (2*%s)/3 AND (4*%s)/3 ) AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months, block_name)    
 
       rows = execute_query(query)
 
       if str(rows) == '[]':
         question  = 'done'
         block_name = 'test_summary'
+        target_age = 'no_target'
 
       else:
         question = str(rows[0][0].encode("utf-8"))
         block_name = str(rows[0][1].encode("utf-8"))
+        target_age = str(rows[0][2].encode("utf-8"))
 
-    return str(question) + '///' + str(block_name)
+    return str(question) + '///' + str(block_name) + '///' + str(target_age)
 
 
 
@@ -365,11 +367,20 @@ def next_test_selection(dob,name):
 def run_test():
     dob = request.args.get('Synni_kuupaev')
     name = request.args.get('Lapse_eesnimi')
+    date_object = datetime.strptime(dob, "%Y-%m-%d").date()
+    age = date.today() - date_object
+    age_months = numeric(int(age.days)/30)
 
     selected_test = next_test_selection(dob = dob, name = name)
 
     question = str(selected_test.split("///")[0])
     block_name = str(selected_test.split("///")[1])
+    target_age = numeric(selected_test.split("///")[2])
+    variance = 1
+
+    cdf = lognorm(age_months, target_age, variance)
+
+    
     data = str(question) + "//" + str(block_name)
 
     if question == "done":
@@ -387,7 +398,7 @@ def run_test():
               "type": "template",
               "payload": {
                 "template_type": "button",
-                "text": question.decode("utf-8"),
+                "text": question.decode("utf-8") + " ; " + str(cdf),
                 "buttons": [
                   {
                     "set_attributes": 
@@ -430,11 +441,12 @@ def run_test():
 
 
 
-@app.route('/lognorm', methods=['GET'])
-def lognorm():
-  x = float(request.args.get('x'))  
-  mean = float(request.args.get('mean'))  
-  var = float(request.args.get('var'))  
+# @app.route('/lognorm', methods=['GET'])
+# def lognorm():
+def lognorm(x, mean, var):
+#  x = float(request.args.get('x'))  
+#  mean = float(request.args.get('mean'))  
+#  var = float(request.args.get('var'))  
 
   mu = float(math.log((mean**2) / math.sqrt(var + mean**2) ))
   sigma = float(math.sqrt(math.log(var / mean**2 + 1)))
@@ -442,6 +454,12 @@ def lognorm():
   p = 0.5 + 0.5*math.erf(a)
 
   return str(p)
+
+
+
+
+
+
 
 @app.route('/pg_data', methods=['GET'])
 def pg_data():
