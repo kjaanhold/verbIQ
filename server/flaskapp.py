fd22145af3a5aaf2b361892ef101036553c268ee
Taavi <taavi.tamkivi@gmail.com>
2017-10-09 22:26:12 +0300

    debug

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -592,7 +592,7 @@ def tests_summary():
       data_jah = str(return_test_results(name, 'Jah').split("///")[0])
       data_ei = str(return_test_results(name, 'Ei').split("///")[0])
       data_ei_tea = str(return_test_results(name, 'Ei tea').split("///")[0])
-      score = str(return_test_results(name, 'Ei').split("///")[1])
+      score = str(round(float(return_test_results(name, 'Ei').split("///")[1])*100))
 
       if (str(data_jah) != 'no_results' and str(data_ei) == 'no_results'):
         data = {
