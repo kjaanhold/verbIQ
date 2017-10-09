f38c4f4dd7b895202cd353d070fa3f58a396177e
Taavi <taavi.tamkivi@gmail.com>
2017-10-09 22:23:24 +0300

    de

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -621,6 +621,7 @@ def tests_summary():
             "messages": [
               {"text": str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah + ","},
               {"text": "aga " + str(name) + " ei oska eriti veel ise " + data_ei + "."},
+              {"text":  str(name) + " skoor on " + score + "."},
               {
                 "attachment": {
                   "type": "template",
