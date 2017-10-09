24f8f103d1b24faecfa025520158ff50c488166c
Taavi <taavi.tamkivi@gmail.com>
2017-10-09 15:14:16 +0300

    debug

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -312,11 +312,11 @@ def to_json(inst, cls):
     return json.dumps(d)
 
 
-@app.route('/next_test_selection', methods=['GET'])
-#def next_test_selection(dob,name):
-def next_test_selection():
-    dob = request.args.get('Synni_kuupaev')
-    name = request.args.get('Lapse_eesnimi')
+#@app.route('/next_test_selection', methods=['GET'])
+def next_test_selection(dob,name):
+#def next_test_selection():
+#    dob = request.args.get('Synni_kuupaev')
+#    name = request.args.get('Lapse_eesnimi')
 
     date_object = datetime.strptime(dob, "%Y-%m-%d").date()
     age = date.today() - date_object
@@ -356,7 +356,7 @@ def next_test_selection():
       else:
         question = str(rows[0][0].encode("utf-8"))
         block_name = str(rows[0][1].encode("utf-8"))
-#        target_age = str(rows[0][2].encode("utf-8"))
+        target_age = str(rows[0][2])
 
     return str(question) + '///' + str(block_name) + '///' + str(target_age)
 
