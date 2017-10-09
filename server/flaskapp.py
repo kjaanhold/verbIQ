456fec144873bdfe5ee90da1b8e753efcbb0e945
Taavi <taavi.tamkivi@gmail.com>
2017-10-09 23:09:28 +0300

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -600,17 +600,18 @@ def tests_summary():
       if (str(data_jah) != 'no_results' and str(data_ei) == 'no_results'):
         data = {
             "messages": [
+              {"text": str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah + "."},
               {
                 "attachment": {
                   "type": "template",
                   "payload": {
                     "template_type": "button",
-                    "text": str(name) + " on " + age_months + " kuu vanune ja ta oskab " + data_jah,
+                    "text":  str(name) + " skoor on " + score + " (keskmise lapse skoor selles vanuses on 100).",
                     "buttons": [
                       {
                         "type": "show_block",
-                        "block_name": "test recurring tests 2",
-                        "title": "Tagasi testima"
+                        "block_name": "returning_parents",
+                        "title": "Tagasi"
                       }
                     ]
                   }
@@ -678,8 +679,8 @@ def tests_summary():
                     "buttons": [
                       {
                         "type": "show_block",
-                        "block_name": "test recurring tests 2",
-                        "title": "Tagasi testima"
+                        "block_name": "returning_parents",
+                        "title": "Tagasi"
                       }
                     ]
                   }
