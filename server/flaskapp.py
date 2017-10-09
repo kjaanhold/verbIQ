b51dcf4a70ed601f6a9b05b7401ba0cf7922b6b7
Taavi <taavi.tamkivi@gmail.com>
2017-10-09 15:17:12 +0300

    debug

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -368,16 +368,16 @@ def next_test_selection(dob,name):
 def run_test():
     dob = request.args.get('Synni_kuupaev')
     name = request.args.get('Lapse_eesnimi')
-    date_object = datetime.strptime(dob, "%Y-%m-%d").date()
-    age = date.today() - date_object
-    age_months = numeric(int(age.days)/30)
+ #   date_object = datetime.strptime(dob, "%Y-%m-%d").date()
+ #   age = date.today() - date_object
+ #   age_months = numeric(int(age.days)/30)
 
     selected_test = next_test_selection(dob = dob, name = name)
 
     question = str(selected_test.split("///")[0])
     block_name = str(selected_test.split("///")[1])
-    target_age = numeric(selected_test.split("///")[2])
-    variance = 1
+#    target_age = numeric(selected_test.split("///")[2])
+#    variance = 1
 
     cdf = str("a") # lognorm(age_months, target_age, variance)
 
