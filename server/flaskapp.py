0bd752871cf8b29610aac45dc1ab64eb8821b693
Taavi <taavi.tamkivi@gmail.com>
2017-10-10 21:26:56 +0300

    d

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -393,7 +393,7 @@ def to_json(inst, cls):
 
 
 #@app.route('/next_test_selection', methods=['GET'])
-def next_test_selection(dob,name):
+def next_test_selection(dob,name,turn):
 #def next_test_selection():
 #    dob = request.args.get('Synni_kuupaev')
 #    name = request.args.get('Lapse_eesnimi')
@@ -412,9 +412,35 @@ def next_test_selection(dob,name):
       block_name = str(rows[0][1].encode("utf-8"))
       target_age = str(rows[0][2])
 
-    else:
+    elif (turn == "return"):
       # this kid has done at least one test
       data = TestResults.query.filter_by(lapse_eesnimi = name, result_value = 'Jah').all()
+      result_dict = [u.__dict__ for u in data]
+      block_name = [d.get('block_name') for d in result_dict]
+      block_name = str(block_name)
+      block_name = block_name.replace('u"','')
+      block_name = block_name.replace('"','')
+#      block_name = block_name.replace("u'","'")
+      block_name = block_name.replace('[','')
+      block_name = block_name.replace(']','')
+ 
+      query = "SELECT t.description, t.block_name, m.target_age FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone WHERE ( m.target_age BETWEEN (2*%s)/3 AND (4*%s)/3 ) AND t.block_name NOT IN (%s) ORDER BY RANDOM() LIMIT 1;" % (age_months, age_months, block_name)    
+
+      rows = execute_query(query)
+
+      if str(rows) == '[]':
+        question  = 'done'
+        block_name = 'test_summary'
+        target_age = str("0")
+
+      else:
+        question = str(rows[0][0].encode("utf-8"))
+        block_name = str(rows[0][1].encode("utf-8"))
+        target_age = str(rows[0][2])
+
+    else:
+      # this kid has done at least one test
+      data = TestResults.query.filter_by(lapse_eesnimi = name).all()
       result_dict = [u.__dict__ for u in data]
       block_name = [d.get('block_name') for d in result_dict]
       block_name = str(block_name)
@@ -449,7 +475,7 @@ def run_test():
     dob = request.args.get('Synni_kuupaev')
     name = request.args.get('Lapse_eesnimi')
 
-    selected_test = next_test_selection(dob = dob, name = name)
+    selected_test = next_test_selection(dob = dob, name = name, turn = "return")
 
     question = str(selected_test.split("///")[0])
     block_name = str(selected_test.split("///")[1])
