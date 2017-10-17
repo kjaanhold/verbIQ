897bee4fd6d653d40212f4d5856a5f8101495818
Taavi <taavi.tamkivi@gmail.com>
2017-10-17 11:34:37 +0300

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -650,7 +650,7 @@ def return_test_score(name):
       result_cdf_value = [d.get('result_cdf_value') for d in result_dict if d.get('result_cdf_value') >= 0]    
       result_cdf_value = str(round(sum(result_cdf_value)*2*100/len(result_cdf_value)))
 
-      bottom_skills_data = TestResults.query.filter(result_cdf_value > -1.0).order_by(TestResults.result_cdf_value.asc()).limit(3)
+      bottom_skills_data = TestResults.query.filter(result_cdf_value = 0.5).order_by(TestResults.result_cdf_value.asc()).limit(3)
       bottom_result_dict = [u.__dict__ for u in bottom_skills_data]
       bottom_block_name = [d.get('result_cdf_value') for d in bottom_result_dict]    
       bottom_block_name = str(bottom_block_name)
