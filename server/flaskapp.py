537b8506f46365b4550ecb04eb573aa3449e0a97
Taavi <taavi.tamkivi@gmail.com>
2017-10-17 11:47:38 +0300

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -652,7 +652,7 @@ def return_test_score(name):
 
       bottom_skills_data = TestResults.query.filter_by(lapse_eesnimi = name).order_by(TestResults.result_cdf_value.asc()).limit(3)
       bottom_result_dict = [u.__dict__ for u in bottom_skills_data]
-      bottom_block_name = [d.get('result_cdf_value') for d in bottom_result_dict]    
+      bottom_block_name = [d.get('block_name') for d in bottom_result_dict]    
       bottom_block_name = str(bottom_block_name)
 
       bottom_block_name = bottom_block_name.replace('[u"', "")
