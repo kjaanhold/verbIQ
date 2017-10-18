ce1ea8d9ecf6a0598e0f64cbc8357d5df7823fae
Taavi <taavi.tamkivi@gmail.com>
2017-10-18 20:59:22 +0300

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -826,7 +826,7 @@ def propose_exercise():
     bottom_block_name = str(returned_test_score.split("///")[2]) 
     query = "SELECT m.description, e.description_est FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones_exercises me ON ms.key_milestone = me.key_milestone JOIN exercises e ON me.key_exercise = e.id_exercise WHERE t.block_name IN (%s) LIMIT 3;" % (bottom_block_name)    
     rows = execute_query(query)
-    out_text = str(rows[1][1].encode("utf-8"))
+    out_text = str(rows[1][0].encode("utf-8"))
     return str(out_text)
 
 
