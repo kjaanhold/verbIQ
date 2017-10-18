dd210a03a59d7e94b4e94de0b4a34b42d8d4194c
Taavi <taavi.tamkivi@gmail.com>
2017-10-18 21:06:13 +0300

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -824,7 +824,7 @@ def propose_exercise():
     name = request.args.get('Lapse_eesnimi')
     returned_test_score = str(return_test_score(name))
     bottom_block_name = str(returned_test_score.split("///")[2]) 
-    query = "SELECT e.description_est, 'a' FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone JOIN milestones_exercises me ON ms.key_milestone = me.key_milestone JOIN exercises e ON me.key_exercise = e.id_exercise WHERE t.block_name IN (%s) LIMIT 3;" % (bottom_block_name)    
+    query = "SELECT m.description, e.description_est FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone JOIN milestones_exercises me ON ms.key_milestone = me.key_milestone JOIN exercises e ON me.key_exercise = e.id_exercise WHERE t.block_name IN (%s) LIMIT 3;" % (bottom_block_name)    
     rows = execute_query(query)
 
     out_text = str(rows[1][1].encode("utf-8"))
