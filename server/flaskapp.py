1ca4cc1db851abf480a4fbc9d481f244106e4cba
Taavi <taavi.tamkivi@gmail.com>
2017-11-16 11:37:22 +0200

    debug

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -640,7 +640,10 @@ def return_test_results(name, result_value):
       out_text = str(rows[0][0].encode("utf-8"))
     return str(out_text)
 
-def return_test_score(name):
+@app.route('/return_test_score', methods=['GET'])
+def return_test_score():
+#def return_test_score(name):
+    name = request.args.get('Lapse_eesnimi')
 
     if TestResults.query.filter_by(lapse_eesnimi = name).first() is None:
       out_text = "0"
