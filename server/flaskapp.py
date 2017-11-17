88d4ddaab1a495eacebe54431736f0d9906d526f
Taavi <taavi.tamkivi@gmail.com>
2017-11-18 00:17:50 +0200

    a

--- server/flaskapp.py
+++ server/flaskapp.py
@@ -637,11 +637,7 @@ def return_test_results(name, result_value):
     return str(out_text)
 
 
-@app.route('/return_test_score', methods=['GET'])
-def return_test_score():
-#def return_test_score(name):
-
-    name = request.args.get('Lapse_eesnimi')
+def return_test_score(name):
 
     if TestResults.query.filter_by(lapse_eesnimi = name).first() is None:
       out_text = "0"
@@ -827,7 +823,7 @@ def propose_exercise():
     returned_test_score = str(return_test_score(name))
     bottom_block_name = str(returned_test_score.split("///")[2]) 
 
-    query = "SELECT m.description, e.description, e.image_url FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone JOIN milestones_exercises me ON ms.key_milestone = me.key_milestone JOIN exercises e ON me.key_exercise = e.id_exercise WHERE t.block_name IN (%s) AND e.language = 'eng' LIMIT 3;" % (bottom_block_name)    
+    query = "SELECT m.description, e.description, e.image_url FROM tests t JOIN milestone_tests ms ON t.id_test = ms.key_test JOIN milestones m ON ms.key_milestone = m.id_milestone JOIN milestones_exercises me ON ms.key_milestone = me.key_milestone JOIN exercises e ON me.key_exercise = e.id_exercise WHERE t.block_name IN (%s) LIMIT 3;" % (bottom_block_name)    
     rows = execute_query(query)
 
     if (len(rows) == 0):
